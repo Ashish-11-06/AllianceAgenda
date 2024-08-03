d0c87000a13f82ae0734a1c3742c0cddd719662f
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-08-03 12:02:27 +0530

    port no

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const pool = require('./db');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
